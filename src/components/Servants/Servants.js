a142655b25907a22d5ccac852c7fe308717b4dac
dtersoff <daverboy21@gmail.com>
2019-10-18 10:30:37 -0400

    Add filter to ReactTable component

--- src/components/Servants/Servants.js
+++ src/components/Servants/Servants.js
@@ -32,6 +32,11 @@ const Servants = ({ user, alerts, match }) => {
       <div>
         <ReactTable
           data = {servants}
+          filterable
+          defaultFilterMethod={(filter, row, column) => {
+            const id = filter.pivotId || filter.id
+            return row[id] !== undefined ? String(row[id]).toLowerCase().startsWith(filter.value.toLowerCase()) : true
+          }}
           columns={[
             {
               Header: 'Name',
